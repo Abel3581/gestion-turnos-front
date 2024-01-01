badfa34279c721bdf7e7bcc96a6e75b7339633e6
Abel3581 <marzoa3581@gmail.com>
2024-01-01 11:26:05 -0300

    act

--- src/app/components/home/home-routing.module.ts
+++ src/app/components/home/home-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { HealthCenterComponent } from './health-center/health-center.component';
+import { ProfileFormComponent } from './profile-form/profile-form.component';
+
 
 const routes: Routes = [
   {
     path: '',
     children:[
-      {
-        path: 'profile', component: ProfileComponent
-      }
+      { path: 'profile', component: ProfileComponent },
+      { path: 'center', component: HealthCenterComponent},
+      { path: 'form', component: ProfileFormComponent}
     ]
   }
 ];
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HealthCenterComponent } from './health-center/health-center.component';
+import { ProfileFormComponent } from './profile-form/profile-form.component';
 
 
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    HealthCenterComponent,
+    ProfileFormComponent
+
   ],
   imports: [
     CommonModule,
